de59c08aca5475cb11d8cd962705e194fc038477
unknown <arrispower@yandex.ru>
2022-09-12 16:10:00 +0500

    Homework 3 seminar

---
+++ Homework3/Program.cs
@@ -0,0 +1,46 @@
+﻿
+/*
+// Домашняя работа 3 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void TableCubes(int n)
+{
+    int i = 1;
+    while (i <= n)
+    {
+        Console.WriteLine($"Cubes number {i} = {i*i*i}");
+        i++;
+    }
+}
+
+Console.Write("Input number: ");
+int cubes = Convert.ToInt32(Console.ReadLine());
+
+TableCubes(cubes);
+*/
+
+/*
+//Домашняя работа 2 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double PointSpace (double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double dx = x1 - x2;
+    double dy = y1 - y2;
+    double dz = z1 - z2;
+    double Point = Math.Sqrt(dx*dx + dy*dy + dz*dz);
+    return Point;
+}
+
+Console.Write("Input x coordinates 1: ");
+double x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input y coordinates 1: ");
+double y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input z coordinates 1: ");
+double z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input x coordinates 2: ");
+double x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input y coordinates 2: ");
+double y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input z coordinates 2: ");
+double z2 = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Length of segment({x1}, {y1}, {z1}) ({x2}, {y2}, {z2}) equal {PointSpace (x1, y1, z1, x2, y2, z2)}");
+*/
