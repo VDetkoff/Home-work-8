45d31c2232602d9e2a247f8f69b78c3e03998a4f
unknown <arrispower@yandex.ru>
2022-09-24 21:00:13 +0500

    Home work 6

---
+++ Homework6/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+// Домашнее задание 2 Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+void CrossPoint(double k1, double b1, double k2, double b2)
+{
+    double x = (b1-b2)/(k2-k1);
+    double y = (k2*b1-k1*b2)/(k2-k1);
+        if(k1==k2) 
+    Console.Write("Lines do not intersect");
+        else
+    Console.Write($"Permission Intersection Point: ({x}; {y})");
+}
+
+
+
+Console.Write("Input b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+CrossPoint(k1,b1,k2,b2);
+*/
+
+//Домашняя работа 1 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Enter number of numbers: ");
+int n = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Input numbers {i + 1}: ");
+    int x = int.Parse(Console.ReadLine());
+    if (x > 0)
+        count++;
+}
+Console.WriteLine($"Positive numbers: {count}");
